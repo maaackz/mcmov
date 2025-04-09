c5df4fad1b92b318153c2ad8bd265d8d5f4d6f3f
maaackz <maaaaackz@gmail.com>
2025-04-10 07:22:31 +0800

    initial commit/mod template.

---
+++ src/client/java/com/maaackz/mcmov/MinecraftMovieModClient.java
@@ -0,0 +1,10 @@
+package com.maaackz.mcmov;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftMovieModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
